6c77daec0d7f28db408f729d95d4d02ea5621d29
Yang chi KUO <ykuo@e3r6p16.clusters.42paris.fr>
2022-05-10 16:02:52 +0200

    05101602

--- ft_printf/main.c
+++ ft_printf/main.c
@@ -6,13 +6,14 @@
 /*   By: ykuo <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 14:34:15 by ykuo              #+#    #+#             */
-/*   Updated: 2022/05/10 12:37:23 by ykuo             ###   ########.fr       */
+/*   Updated: 2022/05/10 16:01:41 by ykuo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdio.h>
 #include <stdarg.h>
+#include <stdlib.h>
 
 void	ft_putchar(char c, int *len)
 {
@@ -33,32 +34,29 @@ void	ft_putstr(char *s, int *len)
 {
 	int	i;
 
-	if (!(*s))
+	if (!(s))
 		ft_putstr ("(null)", len);
-	i = -1;
-	while (s[++i])
-		ft_putchar (s[i], len);
-}
-
-void	ft_putnbr(int n, int *len)
-{
-	if (n == -2147483648)
-		ft_putstr("-2147483648", len);
 	else
 	{
-		if (n < 0)
-		{
-			ft_putchar('-', len);
-			n *= -1;
-		}
-		if (n > 9)
-		{
-			ft_putnbr(n / 10, len);
-			ft_putnbr(n % 10, len);
-		}
-		else
-			ft_putchar(n + 48, len);
+		i = -1;
+		while (s[++i])
+			ft_putchar (s[i], len);
 	}
+}
+
+void	ft_putnbr(int nbr, int *len)
+{
+	long int	n;
+
+	n = (long) nbr;
+	if (n < 0)
+	{
+		ft_putchar('-', len);
+		n *= -1;
+	}
+	if (n > 9)
+		ft_putnbr(n / 10, len);
+	ft_putchar( (n % 10) + 48, len);
 }
 
 void	func_u(unsigned int n, int *len)
@@ -72,15 +70,11 @@ void	func_u(unsigned int n, int *len)
 		ft_putchar (n + 48, len);
 }
 
-void func_x (int n, int *len, int is_cap)
+void func_x (unsigned int n, char *base, int *len)
 {
-	char *base;
-
-	base = "0123456789abcdef";
-	if (is_cap)
-		base = "0123456789ABCDEF";
-	printf ("[%s]", base);
-	ft_putstr (base, len);
+	if (n > 15)
+		func_x (n / 16, base, len);
+	ft_putchar (base[n % 16], len);
 } 
 
 void	ft_printf_func(const char s, va_list lst, int *len)
@@ -89,14 +83,14 @@ void	ft_printf_func(const char s, va_list lst, int *len)
 		ft_putchar (va_arg (lst,  int), len);
 	else if (s == 's')
 		ft_putstr (va_arg (lst, char *), len);
-	else if (s == 'd')
+	else if (s == 'd' || s == 'i')
 		ft_putnbr (va_arg (lst, int), len);
 	else if (s == 'u')
 		func_u (va_arg (lst, unsigned int), len);
 	else if (s == 'x')
-		func_x (va_arg (lst, int), len, 0);
+		func_x (va_arg (lst, int), "0123456789abcdef", len);
 	else if (s == 'X')
-		func_x (va_arg (lst, int), len , 1);
+		func_x (va_arg (lst, int), "0123456789ABCDEF", len);
 	else if (s == '%')
 		ft_putchar (37, len);
 }
@@ -129,35 +123,26 @@ int	ft_printf (const char *s, ...)
  
 int main(int argc, char *argv[])
 {
-    int a, b, c;
-
-//	a = ft_printf (argv[1], -5, 'c', "string", 13, 14, 15, 16, 17, 18, 19, 20);
-//	printf (" | return: %d", a);
+    int a;
 
     printf("\nEnter value of a in decimal format:");
     scanf("%i", &a);
-    printf("%%d -> %d\n", a);
-    printf("%%i -> %i\n", a);
-    printf("%%u -> %u\n", a);
-    printf("%%x -> %x\n", a);
-    printf("%%X -> %X\n", a);
-    //printf("%%i -> %i\n", a);
-    //printf("%%i -> %i\n", a);
 
-/*    printf("\nEnter value of b in octal format: ");
-    scanf("%i", &b);
-    printf("%%d -> %d\n", b);
-    printf("%%i -> %i\n", b);
- 
-    printf("\nEnter value of c in hexadecimal format: ");
-    scanf("%i", &c);
-    printf("%%d -> %d\n", c);
-    printf("%%i -> %i\n", c);
+    printf("printf: %%d -> %d | ", a);
+    ft_printf("ft_printf: %%d -> %d\n", a);
+    printf("printf: %%i -> %i | ", a);
+    ft_printf("ft_printf: %%i -> %i\n", a);
+    printf("printf: %%u -> %u | ", a);
+    ft_printf("ft_printf: %%u -> %u\n", a);
+    printf("printf: %%x -> %x | ", a);
+    ft_printf("ft_printf: %%x -> %x\n", a);
+    printf("printf: %%X -> %X | ", a);
+    ft_printf("ft_printf: %%X -> %X\n", a);
+    printf("printf: %%i -> %i | ", a);
+    ft_printf("ft_printf: %%i -> %i\n", a);
 
-    printf("\n\n Variadic functions: \n");
-    printf("\n 1 + 2 = %d ",AddNumbers(2, 1, 2));
-    printf("\n 3 + 4 + 5 = %d ", AddNumbers(3, 3, 4, 5));
-    printf("\n 6 + 7 + 8 + 9 = %d ", AddNumbers(4, 6, 7, 8, 9)); 
-    printf("rer%q", 2);*/
+	char *s = NULL;
+    printf("printf: %%s -> %s | ", s);
+    ft_printf("printf: %%s (null) -> %s \n", s);
     return 0;
 }
