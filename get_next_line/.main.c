3e1e4eb6d0cb4bfb859e3a9823fa9f8d642cc9eb
Chenade <chenade0312@gmail.com>
2022-08-29 18:48:57 +0200

    get_next_line | Validated - 125%

--- get_next_line/.main.c
+++ get_next_line/.main.c
@@ -6,7 +6,7 @@
 /*   By: ykuo <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 15:44:37 by ykuo              #+#    #+#             */
-/*   Updated: 2022/05/13 13:58:52 by ykuo             ###   ########.fr       */
+/*   Updated: 2022/08/29 18:01:23 by ykuo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,38 +19,26 @@
 int	main(int argc, char *argv[])
 {
 	int		i;
-	int		fd;
+	int		fd = 1;
 	char	*l;
 
 	i = 1;
-	//while (1)
-//	{
-//		if (argc == 1)
-//			fd = 0;
-//		else
-			fd = open (argv[i++], O_RDONLY, 0);
-		if (fd >= 0)
-		{
-			printf("File opened successfully!\n");
+	if (argc == 1)
+		fd = 0;
+	else
+		fd = open (argv[i++], O_RDONLY, 0);
+	if (fd >= 0)
+	{
+		printf("File opened successfully!\n");
+		while (1){
 			l = get_next_line (fd);
+			if (!l)
+				break ;
 			printf("%s", l);
 			free (l);
-			l = get_next_line (fd);
-			printf("%s", l);
-			free (l);
-			l = get_next_line (fd);
-			printf("%s", l);
-			free (l);
-			l = get_next_line (fd);
-			printf("%s", l);
-			free (l);
-			close (fd);
 		}
-		else
-			printf("File opened failed");
-//		if (--argc <= 1)
-//			break ;
-//	}
-	(void) argc;
+		close (fd);
+	}
+	// (void) argc;
 	return (0);
 }
