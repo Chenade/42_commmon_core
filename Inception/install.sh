5c4d939e1496beee96dc2e8ff479cfabd0a16b8e
Live session user <live@core>
2023-04-06 03:10:37 +0000

    update docker installation

--- Inception/install.sh
+++ Inception/install.sh
@@ -1,9 +1,17 @@
+sudo apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common
+
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+sudo echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list
+
 sudo apt-get update
-sudo apt -y install apt-transport-https ca-certificates curl gnupg2 software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-sudo apt update
-sudo apt install -y docker-ce docker-ce-cli containerd.io
-sudo apt-get install docker-compose
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+sudo systemctl enable docker
+
 sudo chmod 666 /var/run/docker.sock
+
+sudo systemctl enable docker
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
